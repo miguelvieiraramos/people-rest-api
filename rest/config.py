688503960be38523c2a642897a529bfc5f1b342b
Miguel Vieira Ramos <53235466+miguelvieiraramos@users.noreply.github.com>
2020-04-29 19:51:29 -0300

    Update config.py

--- rest/config.py
+++ rest/config.py
@@ -1,3 +1,3 @@
 class Config:
     SECRET_KEY = '}lns.+~i(v}170;x8(@l>l2c$cW?M;'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:xxwhe222@localhost/people'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost/people'
